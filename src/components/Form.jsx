d4e4e4537e2cbf642ac74986a6a0dde1867d6968
Renan Fernandes <renzinestuods@gmail.com>
2022-07-25 16:24:03 -0300

    Renderiza o componente AtrributeInput no Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import './Form.css';
+import AtrributeInput from './AtrributeInput';
 
 class Form extends Component {
   render() {
     return (
       <form action="">
-        l
+        <h2>Adicionar Nova Carta</h2>
+        <div>
+          <p>Nome</p>
+          <input
+            type="text"
+            name="cardName"
+            required
+            maxLength="20"
+            id="card-name"
+            data-testid="name-input"
+          />
+        </div>
+        <div>
+          <p>Descrição</p>
+          <textarea
+            name="cardDescription"
+            id="card-description"
+            cols="30"
+            rows="10"
+            required
+            data-testid="description-input"
+          />
+        </div>
+        <div>
+          <AtrributeInput
+            atrributeName="Podersinho"
+            name="attr1"
+            data-testid="attr1-input"
+          />
+          <AtrributeInput
+            atrributeName="Força"
+            name="attr2"
+            data-testid="attr2-input"
+          />
+          <AtrributeInput
+            atrributeName="Velocidade"
+            name="attr3"
+            data-testid="attr3-input"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="image-input"
+            data-testid="image-input"
+            id="image-input"
+          />
+        </div>
       </form>
     );
   }
