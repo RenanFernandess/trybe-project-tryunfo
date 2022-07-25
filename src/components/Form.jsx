c701f1655b49c49f1fa314f4bef50b6766495505
Renan Fernandes <renzinestuods@gmail.com>
2022-07-25 15:28:12 -0300

    Cria um componente form

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <form action="">
+        l
+      </form>
+    );
+  }
+}
+
+export default Form;
